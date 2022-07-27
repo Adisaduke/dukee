8814c0ecc5d0b6786d6c223cb6826c5c31edb151
Adisaduke <rasakadisa.fcaib@gmail.com>
2022-07-27 09:10:30 +0100

    new commit

--- src/components/Works.js
+++ src/components/Works.js
@@ -5,6 +5,7 @@ import food from './assets/food.jpg'
 import Expenses from './assets/Expenses.png'
 import keeperr from './assets/keeperr.png'
 import drumkiit from './assets/drumkiit.png'
+import Trivia from './assets/Trivia.PNG'
 import './styles/Works.scss'
 import { Link } from 'react-router-dom'
 import { ArrowBack, FontDownloadSharp } from '@material-ui/icons'
@@ -20,36 +21,25 @@ const Works = () => {
             <div className="work__section">
 
                 <div className="cards">
-                    <img src={react} alt="" />
-                    <a href="https://www.gmail.com" target="_blank" className='social_accounts'><button>See Work</button></a>
+                    <img src={Trivia} alt="" />
+                    <a href="https://ten-trivia.netlify.app/" target="_blank" className='social_accounts'><button>See Work</button></a>
                     
                 </div>
 
                 <div className="cards">
-                    <img src={ui} alt="" />
-                    <a href="https://www.gmail.com" target="_blank" className='social_accounts'><button>See Work</button></a>
-                </div>
-
-                <div className="cards">
-                   <img src={food} alt="" />
-                   <a href="https://www.gmail.com" target="_blank" className='social_accounts'><button>See Work</button></a>
+                   <img src={Expenses} alt="" />
+                   <a href="https://mellow-youtiao-25bc83.netlify.app/" target="_blank" className='social_accounts'><button>See Work</button></a>
                 </div>
 
                 <div className="cards">
                    <img src={drumkiit} alt="" />
-                   <a href="https://www.gmail.com" target="_blank" className='social_accounts'><button>See Work</button></a>
+                   <a href="https://leafy-concha-ae444d.netlify.app/" target="_blank" className='social_accounts'><button>See Work</button></a>
                 </div>
 
                 <div className="cards">
                    <img src={keeperr} alt="" />
-                   <a href="https://www.gmail.com" target="_blank" className='social_accounts'><button>See Work</button></a>
+                   <a href="https://pogqj.csb.app/" target="_blank" className='social_accounts'><button>See Work</button></a>
                 </div>
-
-                <div className="cards">
-                   <img src={Expenses} alt="" />
-                   <a href="https://www.gmail.com" target="_blank" className='social_accounts'><button>See Work</button></a>
-                </div>
-
             </div>
         </div>
     )
